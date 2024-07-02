f370068391f19cb2ac9f08040df45ad0a54e5e38
Joan Rojas <i202112603@cibertec.edu.pe>
2024-07-02 13:42:07 -0500

    first commit

---
+++ src/main/java/dsw3/t3/joanrojasrodriguez/JoanrojasrodriguezApplication.java
@@ -0,0 +1,13 @@
+package dsw3.t3.joanrojasrodriguez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoanrojasrodriguezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoanrojasrodriguezApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dsw3/t3/joanrojasrodriguez/JoanrojasrodriguezApplicationTests.java
@@ -0,0 +1,13 @@
+package dsw3.t3.joanrojasrodriguez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoanrojasrodriguezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
